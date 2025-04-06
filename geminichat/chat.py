b393f74294f8fa9db90629e3e1003d84d60e8bb5
ShivaniBS <22c19.shivanib@sjec.ac.in>
2025-04-06 16:46:07 +0530

    chat

--- geminichat/chat.py
+++ geminichat/chat.py
@@ -1,8 +1,12 @@
 import os
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 import google.generativeai as genai
 from dotenv import load_dotenv
 
 load_dotenv() 
+app= Flask(__name__)
+CORS(app)
 
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 
