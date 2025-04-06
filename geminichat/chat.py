5e1fa21dfa29c323110189b03c9658ec4f3ed289
ShivaniBS <22c19.shivanib@sjec.ac.in>
2025-04-06 16:37:00 +0530

    Add chat.py and update .gitignore

---
+++ geminichat/chat.py
@@ -0,0 +1,38 @@
+import os
+import google.generativeai as genai
+from dotenv import load_dotenv
+
+load_dotenv() 
+
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+
+# Create the model
+generation_config = {
+  "temperature": 0.7,
+  "top_p": 0.95,
+  "top_k": 40,
+  "max_output_tokens": 8192,
+  "response_mime_type": "text/plain",
+}
+
+model = genai.GenerativeModel(
+  model_name="gemini-1.5-pro",
+  generation_config=generation_config,
+  system_instruction="you are  an expert in carreer guiding for young and enthusiastic women in the field of technology\n",
+)
+history=[]
+print("BOT: Hello! I am your AI assistant. How can I help you today?")
+while True:
+    user_input = input("YOU: ")
+
+    chat_session = model.start_chat(
+       history=history
+    )
+
+    response = chat_session.send_message(user_input)
+
+    model_response=response.text
+    print(f'BOT:{model_response}')
+    print()
+    history.append({"role": "user", "parts": [user_input]})
+    history.append({"role": "model", "parts": [model_response]})
